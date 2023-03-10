63eba54e17f0ab5e086b1cf8ef617d62466ea446
Mayur Garde <mayurgarde777@gmail.com>
2023-03-10 19:49:17 +0530

    Practice2.java file added in ArrayList-Practice folder

---
+++ Java_Collection_Framework/src/Array_List_Practice/Practice2.java
@@ -0,0 +1,27 @@
+package Array_List_Practice;
+
+import java.util.*;
+
+public class Practice2 {
+    public static ArrayList <String> getData() {
+        ArrayList <String> names = new ArrayList <String> ();
+        System.out.print("\nEnter number of strings you wants to store in ArrayList : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++)
+            names.add(sc.next());
+        sc.close();
+        return names;
+    }
+    public static void main(String args[]) {
+        ArrayList <String> name = new ArrayList<String>();
+        name.add("abc");
+        //Adding input strings from user through getData method
+        name.addAll(getData());
+        //Display all Strings 
+        for(String i: name)
+        {
+            System.out.println(i);
+        }
+    }
+}
